8420f89408e79505f89e602178bb5f019c4e9d23
dzshao <danielzhshao@gmail.com>
2025-03-20 20:22:05 -0700

    Added sample traces

--- src/main.cpp
+++ src/main.cpp
@@ -17,13 +17,18 @@ using std::string;
 using std::stringstream;
 using std::pair;
 using std::setprecision;
+using std::fixed;
+using std::to_string;
 
 pair<double, uint64_t> search (const vector<datapoint> &, bool);
 double kFoldValidation (const vector<datapoint> &, uint64_t, int, bool);
 double calcDistance(const datapoint &, const datapoint &, uint64_t);
-
+string convertBitfieldToFeatureSet(uint64_t, int);
 
 int main() {
+    
+    cout << "Searching large dataset..." << endl << endl;
+
     ifstream fin = ifstream("./data/CS170_Large_Data__1.txt");
     if (!fin) {
         cout << "Error opening file." << endl;
@@ -39,15 +44,14 @@ int main() {
         allPoints.push_back({});
 
         sin >> allPoints.back().classification;
-        while (sin >> setprecision(15) >> currFeature) {
+        while (sin >> currFeature) {
             allPoints.back().features.push_back(currFeature);
         }
     }
 
     int size = allPoints.at(0).features.size();
-    cout << "Searching large dataset..." << endl << endl;
     pair<double, uint64_t> result = search(allPoints, true);
-    cout << "Best accuracy with forward search: " << result.first << endl << "Best features: ";
+    cout << "Finished Search!! The best feature subset is {" << convertBitfieldToFeatureSet(result.second, size) << "}, which has an accuracy of " << fixed << setprecision(1) << result.first * 100 << "%";
     for (int i = 0; i < size; ++i) {
         if (result.second & (uint64_t(1) << i)) { 
             cout << i + 1 << ", ";
@@ -56,12 +60,7 @@ int main() {
     cout << endl << endl;
 
     pair<double, uint64_t> backwardResult = search(allPoints, false);
-    cout << "Best accuracy with backward search: " << backwardResult.first << endl << "Best features: ";
-    for (int i = 0; i < size; ++i) {
-        if (backwardResult.second & (uint64_t(1) << i)) { 
-            cout << i + 1 << ", ";
-        }
-    }
+    cout << "Finished Search!! The best feature subset is {" << convertBitfieldToFeatureSet(backwardResult.second, size) << "}, which has an accuracy of " << fixed << setprecision(1) << backwardResult.first * 100 << "%";
     cout << endl << endl;
     
     cout << "Searching small dataset..." << endl << endl;
@@ -81,29 +80,18 @@ int main() {
         allPoints2.push_back({});
 
         sin >> allPoints2.back().classification;
-        while (sin >> setprecision(15) >> currFeature2) {
+        while (sin >> currFeature2) {
             allPoints2.back().features.push_back(currFeature2);
         }
     }
 
     int size2 = allPoints2.at(0).features.size();
-    cout << "Searching large dataset..." << endl << endl;
     pair<double, uint64_t> result2 = search(allPoints2, true);
-    cout << "Best accuracy with forward search: " << result2.first << endl << "Best features: ";
-    for (int i = 0; i < size2; ++i) {
-        if (result2.second & (uint64_t(1) << i)) { 
-            cout << i + 1 << ", ";
-        }
-    }
+    cout << "Finished Search!! The best feature subset is {" << convertBitfieldToFeatureSet(result2.second, size2) << "}, which has an accuracy of " << fixed << setprecision(1) << result2.first * 100 << "%";
     cout << endl << endl;
 
     pair<double, uint64_t> backwardResult2 = search(allPoints2, false);
-    cout << "Best accuracy with backward search: " << backwardResult2.first << endl << "Best features: ";
-    for (int i = 0; i < size2; ++i) {
-        if (backwardResult2.second & (uint64_t(1) << i)) { 
-            cout << i + 1 << ", ";
-        }
-    }
+    cout << "Finished Search!! The best feature subset is {" << convertBitfieldToFeatureSet(backwardResult2.second, size2) << "}, which has an accuracy of " << fixed << setprecision(1) << backwardResult2.first * 100 << "%";
     cout << endl << endl;
 
     return 0;
@@ -114,11 +102,13 @@ Set forwardBackward to true to use forward search, false for backward
 */
 pair<double, uint64_t> search (const vector<datapoint> &data, bool forwardBackward) {
     int size = data.at(0).features.size();
-
+    
+    cout << "Beginning search..." << endl << endl;
     // Use bitfield to represent which features are included
     uint64_t includedFeatures = 0;
     uint64_t bestFeatures = 0;
     double overallBestAccuracy = 0.0;
+
     // Include all features with backwards search
     if (!forwardBackward) {
         includedFeatures = pow(2, data.at(0).features.size()) - 1;
@@ -127,7 +117,7 @@ pair<double, uint64_t> search (const vector<datapoint> &data, bool forwardBackwa
 
     for (int i = 0; i < size; ++i) {
         
-        cout << "Level: " << i + 1 << endl;
+        // cout << "Level: " << i + 1 << endl;
         int bestFeature = 0;
         double bestAccuracy = 0.0;
         
@@ -143,9 +133,9 @@ pair<double, uint64_t> search (const vector<datapoint> &data, bool forwardBackwa
                     continue;
                 }
             }
-            
+            // Find accuracy with including/excluding feature
             double currAccuracy = kFoldValidation(data, includedFeatures, j, forwardBackward);
-            // cout << "Trying feature: " << j + 1 << " Accuracy: " << currAccuracy << endl;
+            cout << "\tUsing feature(s) {" << j + 1 << "} accuracy is " << fixed << setprecision(1) << currAccuracy * 100 << "%" << endl;
             if (currAccuracy >= bestAccuracy) {
                 bestFeature = j;
                 bestAccuracy = currAccuracy;
@@ -154,18 +144,21 @@ pair<double, uint64_t> search (const vector<datapoint> &data, bool forwardBackwa
 
         if (forwardBackward) { 
             includedFeatures = includedFeatures | (uint64_t(1) << bestFeature);
-            cout << "Added feature: " << bestFeature + 1 << endl;
         } else {
             includedFeatures = includedFeatures ^ (uint64_t(1) << bestFeature);
-            cout << "Removed feature: " << bestFeature + 1 << endl;
         }
-        // printBinary(includedFeatures);
+        
+        cout << endl << "Feature set {" << convertBitfieldToFeatureSet(includedFeatures, size) << "} was best, accuracy is " << fixed << setprecision(1) << bestAccuracy * 100 << "%" << endl;
+
         if (overallBestAccuracy < bestAccuracy) {
             overallBestAccuracy = bestAccuracy;
             bestFeatures = includedFeatures;
+        } else {
+            cout << "(Warning, Accuracy has decreased! Continuing search in case of local maxima)" << endl;
         }
-        
-        cout << "Current best accuracy: " << overallBestAccuracy << endl << endl;
+        // cout << endl << "Feature set {" << convertBitfieldToFeatureSet(bestFeatures, size) << "} was best, accuracy is " << fixed << setprecision(1) << overallBestAccuracy * 100 << "%";// << endl;
+        // cout << fixed << setprecision(1) << overallBestAccuracy * 100 << "%" << endl;
+        cout << endl;
     }
     return {overallBestAccuracy, bestFeatures};
 }
@@ -184,10 +177,12 @@ double kFoldValidation (const vector<datapoint> &data, uint64_t features, int ne
         currFeatures = features ^ (uint64_t(1) << newFeature);
     }
 
+    // Iterate through all datapoints leaving out one each time
     for (int hiddenData = 0; hiddenData < size; ++hiddenData) {
         double nearestNeighborClass = 0.0;
         double nearestNeighborDistance = DBL_MAX;
         double currDistance = 0.0;
+        // Find closest neighbor
         for (int j = 0; j < size; ++j) {
             // Don't compare with self
             if (hiddenData == j) {
@@ -210,10 +205,24 @@ double kFoldValidation (const vector<datapoint> &data, uint64_t features, int ne
 double calcDistance(const datapoint &lhs, const datapoint &rhs, uint64_t features) {
     int size = lhs.features.size();
     double sum = 0.0;
+    // Find distance between two points
     for (int i = 0; i < size; ++i) {
         if (features & (uint64_t(1) << i)) {
             sum += pow(fabs(lhs.features.at(i) - rhs.features.at(i)), 2.0);
         }
     }
     return sqrt(sum);
+}
+
+string convertBitfieldToFeatureSet(uint64_t bitfield, int size) {
+    string result = "";
+    for (int i = 0; i < size; ++i) {
+        if (bitfield & (uint64_t(1) << i)) { 
+            result += to_string(i + 1) + ", ";
+        }
+    }
+    if (result.size() > 0) {
+        return result.substr(0, result.size() - 2);
+    }
+    return "";
 }
